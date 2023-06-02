ab0a09a4e0545d258aeaf549a65308bee036c069
Filip Sakala <filip.sakala@vacuumlabs.com>
2023-06-02 13:48:51 +0200

    feat: add CORS and helmet MW

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
 
 import exchangeRates from "./routes/exchangeRates";
 
 const app = express();
 const port = 3200;
 
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use("/exchange-rates", exchangeRates);
+
+app.use((_, res, __) => {
+  res.status(404).send("Sorry can't find that!");
+});
 
 app.listen(port, () => {
   console.log(`😻 Exchange Rates server app listening on port ${port}`);
